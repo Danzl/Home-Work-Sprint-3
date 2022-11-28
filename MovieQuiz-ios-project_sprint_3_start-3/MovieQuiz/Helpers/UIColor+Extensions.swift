6da2f7295cdf9feac2f29021a5566f7aef025681
Danzl <115153894+Danzl@users.noreply.github.com>
2022-11-29 00:18:18 +0500

    Задания по 5-му спринту
    Логика работы приложения разбита на несколько частей.
    Работа по генерации вопросов для квиза вынесена в отдельную сущность.
    Часть логики приложения вынесена в отдельный класс ResultAlertPresenter.
    Обработка нажатия на кнопку алерт реализована.
    Реализована функция сохранения лучшего результата store (с проверкой на то, что новый лучший результат сохранён в User Defaults). К самому алерту в поле message передан сформированный из рекордной игры текст.
    Приложение выглядит корректно — элементы интерфейса адаптируются под разрешения экранов iPhone, начиная с X.
    По окончании раунда вызывается алерт, который соответствует макету и на котором отображаются следующие данные:
    Текущий результат в виде соотношения правильно отвеченных в раунде вопросов и общего количества вопросов в одном раунде
    Количество сыгранных квизов за всё время, что приложение установлено
    Лучший результат за всё время, что приложение установлено. В той же строке должна выводится дата достижения этого результата в формате согласно макетам
    Средняя точность, рассчитывающаяся как отношение правильно отвеченных вопросов за всё время игры к общему количеству вопросов за всё время игры. Точность должна быть выражена в процентах.
    Класс StatisticServiceImplementation закрыт протоколом StatisticService. В MovieQuizViewController используется переменная statisticService типа StatisticService.
    В коде нет ретейн циклов.
    При завершении приложения результаты, которые показываются в конце раунда, не обнуляются.

---
+++ MovieQuiz-ios-project_sprint_3_start-3/MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor? { UIColor(named: "ypBlack") }
+    static var ypWhite: UIColor? { UIColor(named: "ypWhite") }
+    static var ypGreen: UIColor? { UIColor(named: "ypGreen") }
+    static var ypGray: UIColor? { UIColor(named: "ypGray") }
+    static var ypRed: UIColor? { UIColor(named: "ypRed") }
+    static var background: UIColor? { UIColor(named: "background") }
+}
---
+++ MovieQuiz-ios-project_sprint_3_start-3/MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Danzl Wa on 28.11.2022.
+//
+
+import Foundation
+import UIKit
+
+public struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
